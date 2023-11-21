dd146a6aae77dd7ab328e5ebeb1b7a42417ac6a9
Ramazan-Berkan-Tut <129144228+Ramazan-Berkan-Tut@users.noreply.github.com>
2023-11-21 21:36:40 +0300

    Javascript Cover

--- main.js
+++ main.js
@@ -1,93 +1,310 @@
-// Değişkenler
+// export edilen elemanları içe aktrma
+import { leaderTable as table } from './constants.js';
+// varsayılan elemanı import etme
+import sifre from './constants.js';
 
-//------------------------------
-// eski var ; yeni let const değeri sonradan değiştirilir
-// var
-var x=40;
-// let
-let ad='berkan';
-// const değişmez
-const user={
-    name:'berkan',
-    surname:'tut'
+console.log(table);
+console.log(sifre);
+
+export const deneme = 10;
+export const deneme1 = 50;
+export const deneme2 = 60;
+export const deneme3 = 60;
+
+// //! Değişkenler
+
+//* var:
+//* eski sürümlerde kullanılan değişkken tanımlama yöntemidir.
+//* yenisi sürümlerde yerine let ve const kullanılır
+//* değeri sonradan değiştirlebilir
+//* hoisting mekenizması sayesinde değişkeni yukarıya taşır
+//* kullandıktan sonra tanımlayabiliriz
+
+var x = 40;
+x = 40;
+
+//* es6 let
+//* tanımlandığı blok içerisnde kullanılır (block - scope)
+//* değişkeni tanımladan önce kullanmayız
+//* değerini sonradan değiştebiliriz
+let surname = 'Yıldız';
+surname = 'Kaya';
+
+// //* es6 const
+// //* const ile tanımlana değişken sadece bir kez atanabilir
+// //* sonrasında değiştirilemez
+// //* obje veya dizi tanımladnığında kendisi değiştirlemesede
+// //* nesnenin içeriği değişebilir
+
+const user = {
+  name: 'ahmet',
+  surname: 'Kara',
 };
-user.name='ramazan';
+user.name = 'Ali';
 
-const tel=[12 , 23];
-tel[0]=56789;
+const users = ['ali', 'ayşe'];
+console.log(users);
+users[0] = 'Mehmet';
+console.log(users);
 
-console.log(user.name)
-salam();
+// //! Hoisting
 
-function salam() {
-    console.log("selamlar")
+// // javascript'te hoisting kladırma bir dğeişkeni veya fonksiyonu
+// // tanımlanmadan önce kullanılmasına olanak sağlaar
+// // Bazen beklenemdik sonuçlar ortaya çıkabiliyor.
+var foo;
+console.log(foo); // undefined
+
+foo = 70;
+
+console.log(foo); // 70
+
+selamla();
+
+function selamla() {
+  console.log('selam');
 }
 
-//  Veri türleri
-// string
-const name="Selamlar";
+// //! Javascript Veri Türleri
 
-let temel="yeni";
-const uzun=`bunu 
-option ve virgülle yaptım 
-uzun string e yaradı
-ayrıca değişken de alabilirim ${temel}`;
-console.log(uzun);
-// number
-const sayi=5;
-// boolen
-const is_true=true;
-// Object
-const person ={
-    adi:'berkan',
-    soyad:`tester`,
-    okay:is_true,
+// //* String
+let statuss = 'kötü';
+
+const text = "Selam bugün  nasıl' geçiyor ";
+const text2 = 'Gayet güzel, hava "çok" güneşli';
+const text3 = `
+-Javsacirpt öğrendin mi ?
+-Evet gayet ${statuss}.
+`; // template literals > backtick > ``
+
+// //* Number
+const sayi = 4;
+const sayi2 = 4.1;
+
+console.log(sayi + sayi2);
+
+// //* Boolean
+const is_premium = true;
+const is_student = false;
+
+// //* Object (Nesne)
+// //* birden çok özelliği ve değeri içeren karmaşık veri yapılarıdır.
+// //* {} içereinde anahtar - değer çiftleriyle kullanıyoruz
+var person = {
+  name: 'John',
+  age: 40,
+  has_licence: false,
 };
-// Array
-var color=['kırmızı','mavi',true,78];
-// Undefine
+
+// //* Array  (Dizi)
+// //* Sıları öğelerin listesini temsil eder
+var colors = ['kırmızı', true, 59, { name: 'ad' }];
+
+// //* Undefined (Tanımsız)
 var u;
 console.log(u);
-// null bilinçli olarak boş bırakılan yer, böyle bir değer yok kullanıcı girmemiş oluyor
-var nul=null;
 
+// //* Null (Boş)
+// //* Bir değişkenin bilinçli olarak boş bırakılıdğını veya olmadığını temsil eder
+var student = {
+  name: 'Ali',
+  surname: 'kaya',
+  hobby: null,
+};
+console.log(student.hobby);
 
+// //! Function (Fonksiyonlar)
+// //* Belirli bir göre yerine getiren veya sonuç döndüren
+// //* adlandırılmış kod bloklarıdır.
 
-// Funcition 
-t=add(5,6);
-function add(x,y){
-   p= x+y;
-   return p;
+// //* Fonksiyon Adı: İşlevin ne yaptığını açıklayan isim
+// //* Fonksiyonun Gövdesi: Gerçekleştirceği işlevi içeren kod bloğu
+// //* Parmatreler: Fonksiyonun çalışması için dışarıdan gönderilen değerler
+// //* Dönüş Değer (return): yaptığı işlemler sonucunda ürettiği
+// //* sonucu fonksiyonun çağrıldı yere gönderiri
+function toplama(a, b) {
+  const sum = a + b;
+  return sum;
+}
+
+const sum1 = toplama(50, 100); // fonksiyonu çağır ve değeri al
+const sum2 = toplama(6548645, 120000); // parametreler değer atamsı yap
+
+document.write(sum1);
+console.log(sum2);
+
+// //* Arrow Function (Ok Fonksiyon)
+
+function test(name) {
+  return 'selam ' + name;
+}
+
+// //* tek paramtre varsa paranzte gerek yok
+// //* return edilecek veri tek satırdaysa doğrudan yazılır () =>
+const test2 = (name) => 'selam' + name;
+
+// //* return den önce hesaplamalar yappılacaksa () => {}
+const test3 = (a, b) => {
+  let c = a + b;
+  return c;
 };
 
-const testfonk= adiniz=> 'selam' + adiniz; 
-console.log(testfonk('berkan'));
+// //* return edilcek veri çok satırlıysa () => ()
+const test4 = () =>
+  ` <meta charset="UTF-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0"
+    />
+    <link rel="stylesheet" href="style.css" />
+    <title></title>
+  `;
 
-const test3 =(a,b)=> {
-    let c= a*b;
-    return c;
+// //* return edilecek veri obje veya diziyse
+const test5 = () => ({ name: 'ali', age: 760 });
+const test6 = () => {
+  return { name: 'ali', age: 760 };
 };
-console.log(test3(2,4));
 
-// Veri çok satırlıysa
-const test1=(texxt) => (
-`
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-    
-</head>
-<body>
-    <h1>Selam</h1>
+console.log(test5());
 
-    <script src="main.js"></script>
-</body>
-</html>
-`
+// //! Diziler
 
-);
-console.log(test1());
+console.table(table);
+
+//* push: dizinin sonun ekleme
+table.push({
+  id: 'sdxfsdd123',
+  name: 'SonBerkan',
+  point: 5400,
+  isActive: false,
+});
+console.table(table);
+
+//* push: dizinin başına ekleme
+table.unshift({
+  id: 'sdxfsdd23',
+  name: 'Berkan',
+  point: 5400,
+  isActive: false,
+});
+console.table(table);
+
+//*shift() >  dizinin başındaki elemanı çıkart
+const removedItem = table.shift();
+console.log(removedItem);
+console.table(table);
+
+//* pop() >  dizinin sonunda elemanı çıkart
+const removedItem2 = table.pop();
+console.log(removedItem2);
+console.table(table);
+
+//* slice >  diziden bblirli birr parçayı  alma
+//* slice seçtiğimiz elemalardan yeni bir dizi oluşturur
+//* ana dizide bir değişiklik olmaz
+const part = table.slice(4, 6);//başlangıç bitiş
+console.table(part);
+// console.table(table);
+
+//* find
+//* bir özelliğini bildiğimiz elemanı dizide bulmaya yarar
+//* sadece bir eleman döndürür
+
+const foundUser = table.find((user) => user.id === '7asdk901');
+const foundUser2 = table.find((user) =>{ return user.id === '7asdk901'});
+console.log('Bulundu', foundUser);
+console.log('Bulundu', foundUser2);
+//* forEach
+//* bir eylenmi dizideki bütün elemanlar için tekrar etmek
+//* istiyorsak kullanılır. dizideki her bir eleman için
+//* fonk. çalıştırdığı param. olarak elemanı gönderir.
+
+function ekranaBas(user) {
+  console.log(
+    `<b> ${user.name}</b> kullanıcın aldığı puan <b>${user.point}</b> <br>`
+  );
+}
+
+table.forEach(ekranaBas);
+
+//* map
+//* dizideki her bir eleman için fonks çalıştıtır
+//* elemanı çalıştıırığı fonk param olarak gönderiri
+//* çalışan fonk.dan return edilen verilerle yeni dizi oluşur
+
+const decide = (user) => ({
+  ...user, // user objesindeki bütün veileri buraya aktardık
+  name: 'berkan', //user.point + 200,
+  point: user.point+250,
+});
+
+const newTable = table.map(decide);
+
+console.table(table);
+console.table(newTable);
+
+// belirli bir süre sonra bir kere fonk. çalıştırır
+setTimeout(() => console.log('selam'), 3000);
+// belirli süre bir fonk .çalıştırır
+setInterval(() => console.log('selam'), 3000);
+
+/*
+! Promise
+* Asenkron işlemleri temsil etmek için kullan
+* javascript nesnesidir. Promise işlemin sonucunu temsil eder
+* Her asenkron işlemin üç durumu vardır.
+* pending: Bekliyor
+* fulfilled: Olumlu oalrak gerçekleşmesi
+* rejected: hata oluşması
+*/
+
+const apiRequest = new Promise((resolve, reject) => {
+  if (true) {
+    setTimeout(resolve, 3000);
+  } else {
+    setTimeout(reject, 2000);
+  }
+});
+//! Promise Yapıları Almak
+//* 1.yol > then > catch > finally
+apiRequest
+  // ancak promise başarıyla sonuçlanırsa çalışır
+  .then((data) => console.log('Başarıyla sonuçlandı'))
+  // başarasız olarak sonuçlanırsa çalışır
+  .catch((err) => console.log('Başarısız oldu'))
+  // her iki durumda çalışır
+  .finally(() => console.log('APİ İsteği çalıştı'));
+
+//* 2. yol > async > await
+//* promise'leri daha okunaklı hale getiren es6 ile gelen js öözelliği
+//* async > promise dönürüen fonklar tanımlanailir
+//* await > asenkton bir fonksiyon içerisinde promise'in cevap vermesini bekler
+
+async function getData() {
+  try {
+    // kodu çalıştırmayı dener
+    const data = await apiRequest;
+    console.log('api den başarılı cevap geldi');
+  } catch (err) {
+    // hata olursa bu blok çalışır
+    console.log('promise başlarısz oldu');
+  }
+}
+
+getData();
+
+//! Asenkron
+// aseknron terimi: bir işlemin başlatılması ve sonuç alması
+// arasındaa beklemey karar vermeden devam edebilmesi anlamına gelir
+// fetch > http istekleri  atmaya yarar
+
+const getDataa = async () => {
+  const res = await fetch('https://fakestoreapi.com/products');
+  const data = await res.json();
+
+  console.table(data);
+  console.log(res);
+};
+
+getDataa();
